e7f2849eb633b212a5b8d70d7bea71a9866cb99a
Oriol Parra <oriol.parra@forcemanager.net>
2018-06-17 20:13:12 +0200

    refactor: clean comments and refactor
    Move BooksAPI into an 'api' folder. Remove some TODO comments.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI'
 import './App.css';
 import { Route } from 'react-router-dom';
 import ListBooksPage from './containers/list-books-page';
@@ -7,14 +6,6 @@ import SearchBookPage from './containers/search-book-page';
 
 
 class BooksApp extends React.Component {
-	state = {
-		/**
-		 * TODO: Instead of using this state variable to keep track of which page
-		 * we're on, use the URL in the browser's address bar. This will ensure that
-		 * users can use the browser's back and forward buttons to navigate between
-		 * pages, as well as provide a good URL they can bookmark and share.
-		 */
-	}
 
 	render() {
 		return (
