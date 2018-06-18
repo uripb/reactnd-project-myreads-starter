ee5905c5c4fd3fb2ca355d50972c51c488c1e681
Oriol Parra <oriol.parra@forcemanager.net>
2018-06-18 09:08:59 +0200

    refactor: book shelf selector component
    New component to manage "book shelf selector".

--- src/components/book.js
+++ src/components/book.js
@@ -1,39 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BOOK_SHELF } from '../constants';
-
-const activeShelfs = ['currentlyReading', 'wantToRead', 'read'];
+import BookShelfSelector from './book-shelf-selector';
 
 const Book = (props) => {
     const { book, onChangeShelf } = props;
-    const bookShelf = activeShelfs.includes(book.shelf) ? book.shelf : 'none';
-    const imgUrl = (book.imageLinks) ? book.imageLinks.thumbnail : '';
+    const imgUrl = (book.imageLinks) ? `url(${book.imageLinks.thumbnail})` : 'none';
 
     return (
         <li>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${imgUrl}`
-                    }}></div>
-                    <div className="book-shelf-changer">
-                        <select
-                            value={bookShelf}
-                            onChange={(event) => onChangeShelf(book, event.target.value)}
-                        >
-                            <option value="move" disabled>Move to...</option>
-                            {BOOK_SHELF.map(shelf => (
-                                <option
-                                    key={shelf.key}
-                                    value={shelf.key}>
-                                    {shelf.name}
-                                </option>
-                            ))}
-                            <option value="none">None</option>
-                        </select>
+                    <div
+                        className="book-cover"
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: imgUrl
+                        }}>
                     </div>
+                    <BookShelfSelector
+                        value={book.shelf}
+                        onChange={(value) => onChangeShelf(book, value)}
+                    />
                 </div>
                 <div className="book-title">{book.title}</div>
                 <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
